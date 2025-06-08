e3dd0956272f6382845f31c57ffa30777cab1ec6
Hari Maurya <harimaurya879@gmail.com>
2025-06-08 22:10:27 +0530

    bug fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -126,8 +126,9 @@ export const isAuth = async (req, res) => {
 
 export const logout = async (req,res) =>{
     try {
-        res.clearCookie('token',{
+        res.setCookie('token',"",{
             httpOnly: true,
+            maxAge: 0,
             secure: process.env.NODE_ENV === "production",
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", 
           })
